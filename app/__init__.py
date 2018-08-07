3263a87d3c735c235b4c1aac5bb678700577de99
玄月 <huxin39@163.com>
2018-08-07 18:19:02 +0800

    database
    database  db

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlcemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlcemy(app)
+migrate = Migrate(app, db)
 
 from app import routes
-from app import views
+from app import routes, models
--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    test = 00000
-
+    SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 #     OPENID_PROVIDERS = [
 #     { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
 #     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
