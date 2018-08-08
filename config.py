2f06019f917cf6cb3846122e8517d89946f637b2
玄月 <huxin39@163.com>
2018-08-08 18:01:36 +0800

    5 user login

--- config.py
+++ config.py
@@ -3,9 +3,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     OPENID_PROVIDERS = [
+
+#     OPENID_PROVIDERS = [SQLALCHEMY_POOL_RECYCLE
 #     { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
 #     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
 #     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
