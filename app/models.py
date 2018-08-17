fe95292fa8696de97ad6f28ba9027b1e30506952
玄月 <huxin39@163.com>
2018-08-17 11:17:43 +0800

    8 Followers

--- app/models.py
+++ app/models.py
@@ -5,13 +5,27 @@ from app import db, login
 from hashlib import md5
 
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+    )
+
+
 class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index=True, unique=True)
-	email = db.Column(db.String(120), index=True, unique=True)
-	password_hash = db.Column(db.String(128))
+	posts = db.relationship('Post', backref='author', lazy='dynamic')
 	about_me = db.Column(db.String(140))
 	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+	email = db.Column(db.String(120), index=True, unique=True)
+	password_hash = db.Column(db.String(128))
+
+	followed = db.relationship(
+		'User', secondary=followers,
+		primaryjoin=(followers.c.follower_id == id),
+		secondaryjoin=(followers.c.follower_id == id),
+		backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
+	)
 
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
@@ -26,6 +40,28 @@ class User(UserMixin, db.Model):
 		digest = md5(self.email.lower().encode('utf-8')).hexdigest()
 		return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
+	def follow(self, user):
+		if not self.is_following(user):
+			self.followed.append(user)
+
+	def unfollow(self, user):
+		if self.is_following(user):
+			self.followed.remove(user)
+
+	def is_following(self, user):
+		return self.followed.filter(
+			followers.c.follower_id == user.id).count() > 0
+
+	def followed_posts(self):
+		followed = Post.query.join(
+			followers, (followers.c.follower_id == Post.user_id)).filter(followers.c.follower_id == self.id)
+		own = Post.query.filter_by(user_id=self.id)
+		return followed.union(own).order_by(Post.timestamp.desc())
+
+
+@login.user_loader
+def load_user(id):
+	return User.query.get(int(id))
 
 
 class Post(db.Model):
@@ -36,8 +72,3 @@ class Post(db.Model):
 
 	def __repr__(self):
 		return '<Post {}>'.format(self.body)
-
-
-@login.user_loader
-def load_user(id):
-	return User.query.get(int(id))
