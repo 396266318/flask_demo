cc61a78aefddce19ac5bb084aa146aaaefbca161
玄月 <huxin39@163.com>
2018-08-27 14:15:12 +0800

    第十六章：全文搜索 全部

--- config.py
+++ config.py
@@ -18,5 +18,5 @@ class Config(object):
     ADMINS = ['your-email@example.com']
     LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
-    POSTS_PER_PAGE = 25
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    POSTS_PER_PAGE = 25
